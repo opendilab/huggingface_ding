4d26d32194e90291681bee12ad3731684d24d1ff
zjowowen <zjowowen@outlook.com>
2023-06-09 08:16:10 +0000

    Add multiple deploy

--- huggingface_ding/method.py
+++ huggingface_ding/method.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 import numpy as np
 import torch
+import logging
 from pathlib import Path
 from huggingface_hub import ModelCard, ModelCardData
 from huggingface_hub import HfApi
@@ -20,10 +21,10 @@ def _find_video_file_path(record_path):
         if os.path.splitext(p)[-1] == ".mp4":
             file_list.append(p)
     file_list.sort(key=lambda fn: os.path.getmtime(os.path.join(record_path, fn)))
-    assert len(file_list)>=1, "No replay rendered."
-    if len(file_list)==1:
+    assert len(file_list) >= 1, "No replay rendered."
+    if len(file_list) == 1:
         video_path = os.path.join(record_path, file_list[0])
-    else: 
+    else:
         # sometimes the late media file is a one-frame video generated after first to reset and then close the environment.
         video_path = os.path.join(record_path, file_list[-2])
     return video_path
@@ -36,17 +37,34 @@ def _calculate_model_params(model):
         Total_params += mulValue
     return Total_params
 
+
 def _count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+
 def _get_agent_policy_state_dict(agent):
-    if hasattr(agent.policy,"state_dict"):
+    if hasattr(agent.policy, "state_dict"):
         return agent.policy.state_dict()
-    elif hasattr(agent.policy,"_state_dict_learn"):
+    elif hasattr(agent.policy, "_state_dict_learn"):
         return agent.policy._state_dict_learn()
     else:
         raise ValueError("No state_dict method available for this Policy.")
-    
+
+
+def _huggingface_api_upload_file(huggingface_api, path_or_fileobj, path_in_repo, repo_id, retry=5):
+    while retry > 0:
+        try:
+            huggingface_api.upload_file(
+                path_or_fileobj=path_or_fileobj,
+                path_in_repo=path_in_repo,
+                repo_id=repo_id,
+            )
+            break
+        except:
+            retry -= 1
+            assert retry > 0, "Huggingface Hub upload retry exceeded limit."
+
+
 # This method save, evaluate, generate a model card and record a replay video of your agent before pushing the repo to the hub
 def push_model_to_hub(
     agent,
@@ -75,8 +93,10 @@ def push_model_to_hub(
         - algo_name (:obj:`str`): the policy name of the agent.
         - wandb_url (:obj:`str`): the wandb url of the trainning process.
         - repo_id (:obj:`str`): the repository id of Huggingface Hub where the model is stored.
-        - usage_file_by_git_clone (:obj:`str`): the path of a python file which describes ways to use the OpenDILab/DI-engine model that git cloned from huggingface hub.
-        - usage_file_by_huggingface_ding (:obj:`str`): the path of a python file which describes ways to use the OpenDILab/DI-engine model that downloaded by huggingface ding.
+        - usage_file_by_git_clone (:obj:`str`): the path of a python file which describes ways to use the \
+            OpenDILab/DI-engine model that git cloned from huggingface hub.
+        - usage_file_by_huggingface_ding (:obj:`str`): the path of a python file which describes ways to use \
+            the OpenDILab/DI-engine model that downloaded by huggingface ding.
         - train_file (:obj:`str`): the path of a python file which describes how this model is trained.
         - github_repo_url (:obj:`str`): the github url of the DI-engine repository which the model is used.
         - github_doc_model_url (:obj:`str`): the github or document url of the model used.
@@ -89,9 +109,16 @@ def push_model_to_hub(
         huggingface_api = HfApi()
 
         torch.save(_get_agent_policy_state_dict(agent), os.path.join(Path(workfolder), "pytorch_model.bin"))
-        agent.deploy(enable_save_replay=True, replay_save_path=os.path.join(Path(workfolder), 'videos'))
+        best_deploy_return = -np.inf
+        for i in range(10):
+            deploy_return_ = agent.deploy(
+                enable_save_replay=True, replay_save_path=os.path.join(Path(workfolder), f'videos_{i}')
+            )
+            if deploy_return_ > best_deploy_return:
+                best_deploy_return = deploy_return_
+                best_video_path = _find_video_file_path(os.path.join(Path(workfolder), f'videos_{i}'))
         save_config_py(agent.cfg, os.path.join(Path(workfolder), 'policy_config.py'))
-        eval_return=agent.batch_evaluate()
+        eval_return = agent.batch_evaluate()
         with open(os.path.join(Path(workfolder), 'policy_config.py'), 'r') as file:
             python_config = file.read()
         if usage_file_by_git_clone is not None and os.path.exists(usage_file_by_git_clone):
@@ -106,30 +133,30 @@ def push_model_to_hub(
         else:
             usage_by_huggingface_ding = ""
 
-
-
         if train_file is not None and os.path.exists(train_file):
             with open(train_file, 'r') as file:
                 python_code_for_train = file.read()
         else:
             python_code_for_train = ""
 
-        model_size = str(round(_calculate_model_params(_get_agent_policy_state_dict(agent)["model"]) / 256.0, 2)) + " KB"
+        model_size = str(
+            round(_calculate_model_params(_get_agent_policy_state_dict(agent)["model"]) / 256.0, 2)
+        ) + " KB"
 
         if model_description is None:
-            model_description=""
+            model_description = ""
 
         if installation_guide is None:
-            installation_guide=""
+            installation_guide = ""
 
         if github_repo_url is None:
-            github_repo_url="https://github.com/opendilab/DI-engine"
+            github_repo_url = "https://github.com/opendilab/DI-engine"
 
         if github_doc_model_url is None:
-            github_repo_url="https://di-engine-docs.readthedocs.io"
+            github_repo_url = "https://di-engine-docs.readthedocs.io"
 
         if github_doc_env_url is None:
-            github_repo_url="https://di-engine-docs.readthedocs.io"
+            github_repo_url = "https://di-engine-docs.readthedocs.io"
 
         if create_repo:
             huggingface_api.create_repo(
@@ -137,30 +164,32 @@ def push_model_to_hub(
                 private=True,
             )
 
-        model_file_url = huggingface_api.upload_file(
+        model_file_url = _huggingface_api_upload_file(
+            huggingface_api=huggingface_api,
             path_or_fileobj=os.path.join(Path(workfolder), "pytorch_model.bin"),
             path_in_repo="pytorch_model.bin",
             repo_id=repo_id,
         )
 
-        demo_file_url = huggingface_api.upload_file(
-            path_or_fileobj=_find_video_file_path(os.path.join(Path(workfolder), 'videos')),
+        demo_file_url = _huggingface_api_upload_file(
+            huggingface_api=huggingface_api,
+            path_or_fileobj=best_video_path,
             path_in_repo="replay.mp4",
             repo_id=repo_id,
         )
 
-        
-        config_file_url = huggingface_api.upload_file(
+        config_file_url = _huggingface_api_upload_file(
+            huggingface_api=huggingface_api,
             path_or_fileobj=os.path.join(Path(workfolder), 'policy_config.py'),
             path_in_repo='policy_config.py',
             repo_id=repo_id,
         )
 
-        metric=[
+        metric = [
             {
-                "name":"mean_reward",
-                "value":str(round(eval_return.eval_value,2))+" +/- "+str(round(eval_return.eval_value_std,2)),
-                "type":"mean_reward",
+                "name": "mean_reward",
+                "value": str(round(eval_return.eval_value, 2)) + " +/- " + str(round(eval_return.eval_value_std, 2)),
+                "type": "mean_reward",
             }
         ]
 
@@ -172,21 +201,25 @@ def push_model_to_hub(
             task_name=task_name,
             tags=["deep-reinforcement-learning", "reinforcement-learning", "DI-engine", task_name],
             **{
-                "pipeline_tag":"reinforcement-learning",
-                "model-index":[{
-                    "name":algo_name,
-                    "results":[{
-                        "task":{
-                            "name":"reinforcement-learning",
-                            "type":"reinforcement-learning",
-                        },
-                        "dataset":{
-                            "name":'{}-{}'.format(env_name, task_name),
-                            "type":'{}-{}'.format(env_name, task_name),
-                        },
-                        "metrics":metric
-                    },]
-                },]
+                "pipeline_tag": "reinforcement-learning",
+                "model-index": [
+                    {
+                        "name": algo_name,
+                        "results": [
+                            {
+                                "task": {
+                                    "name": "reinforcement-learning",
+                                    "type": "reinforcement-learning",
+                                },
+                                "dataset": {
+                                    "name": '{}-{}'.format(env_name, task_name),
+                                    "type": '{}-{}'.format(env_name, task_name),
+                                },
+                                "metrics": metric
+                            },
+                        ]
+                    },
+                ]
             }
         )
 
@@ -225,7 +258,7 @@ def push_model_to_hub(
             raise ValueError("model card info is invalid. please check.")
 
 
-def pull_model_from_hub(repo_id:str):
+def pull_model_from_hub(repo_id: str):
     """
     Overview:
         Pull public available models from Huggingface Hub
